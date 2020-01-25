41134a766f421b968de5af24654d6c35a28d0263
Juriën Meerlo <codescapade@gmail.com>
2020-01-25 23:17:26 +0000

    Added gitignore and khafile.

---
+++ Sources/Main.hx
@@ -0,0 +1,12 @@
+package;
+
+import kha.System;
+
+class Main {
+
+  static function main(): Void {
+    System.start({ title: 'Quadtree Collision', width: 800, height: 600 }, function(_) {
+
+    });    
+  }
+}
---
+++ khafile.js
@@ -0,0 +1,4 @@
+let project = new Project('Quadtree Collision');
+project.addAssets('Assets');
+project.addSources('Sources');
+resolve(project);
